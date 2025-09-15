07d6ad6209261094ab21f033beaa19211f0b561b
tinsflash <skysnapia@gmail.com>
2025-09-15 14:49:36 +0200

    Update weather.js

--- utils/weather.js
+++ utils/weather.js
@@ -1,17 +1,16 @@
 const axios = require("axios");
 
 async function getWeather(lat, lon) {
-  try {
-    const apiKey = process.env.OPENWEATHER_KEY; // ta clé est dans Render
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=fr`;
+  const apiKey = process.env.OPENWEATHER_API;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=fr`;
 
+  try {
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
-    console.error("Erreur API météo:", error.message);
-    throw new Error("Impossible de récupérer la météo");
+    console.error("Erreur OpenWeather:", error.message);
+    throw new Error("Impossible d'obtenir les données OpenWeather");
   }
 }
 
 module.exports = { getWeather };
-
