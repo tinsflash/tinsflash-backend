9ae1234b3f64322565592cbb202ed953cd1ba608
tinsflash <skysnapia@gmail.com>
2025-09-23 18:02:57 +0200

    Update chatService.js

--- services/chatService.js
+++ services/chatService.js
@@ -1,13 +1,12 @@
-// src/services/chatService.js
 import cohere from "cohere-ai";
 
-cohere.init(process.env.COHERE_API_KEY); // 🔑 ta clé est déjà dans Render
+cohere.init(process.env.COHERE_API_KEY); // 🔑 clé stockée dans Render
 
 async function chatWithJean(message) {
   try {
     const response = await cohere.generate({
-      model: "command-xlarge-nightly", // ✅ modèle IA cohérent & gratuit
-      prompt: `Tu es J.E.A.N., le moteur IA de la Centrale Nucléaire Météo Mondiale, tu es le plus grand expert météorologueet climatologuedu monde,.
+      model: "command-xlarge-nightly", // ✅ modèle IA cohérent
+      prompt: `Tu es J.E.A.N., le moteur IA de la Centrale Nucléaire Météo Mondiale. tubes le plus grand expert météorologue et climatologue du monde.
 Analyse ce message utilisateur et produis une réponse précise, fiable, en rapport avec la météo, prévisions et alertes.
 Message: ${message}`,
       max_tokens: 200,
