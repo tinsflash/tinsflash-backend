74be78f73494f45377dc42070524dc26a2b676a3
tinsflash <skysnapia@gmail.com>
2025-09-24 09:14:41 +0200

    Update Forecast.js

--- models/Forecast.js
+++ models/Forecast.js
@@ -1,17 +1,20 @@
 // models/Forecast.js
 import mongoose from "mongoose";
 
+const ZoneSchema = new mongoose.Schema({
+  name: String,
+  min: Number,
+  max: Number,
+  icon: String,
+  text: String
+});
+
 const ForecastSchema = new mongoose.Schema({
-  timestamp: { type: Date, default: Date.now },
-  location: {
-    lat: { type: Number, required: true },
-    lon: { type: Number, required: true },
-    city: { type: String },
-    country: { type: String }
-  },
-  data: mongoose.Schema.Types.Mixed, // les prévisions fusionnées
-  sources: [String], // liste des modèles utilisés
-  anomaly: mongoose.Schema.Types.Mixed // anomalies saisonnières détectées
+  country: String,
+  zones: [ZoneSchema],
+  text: String,
+  icon: String,
+  createdAt: { type: Date, default: Date.now }
 });
 
 export default mongoose.model("Forecast", ForecastSchema);
