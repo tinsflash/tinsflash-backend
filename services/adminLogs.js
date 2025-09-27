81f12293cc28bf93f307bda1c94b99dd428a3e3d
tinsflash <skysnapia@gmail.com>
2025-09-27 14:46:53 +0200

    Update adminLogs.js

--- services/adminLogs.js
+++ services/adminLogs.js
@@ -1,24 +1,12 @@
 // services/adminLogs.js
-
 let logs = [];
 
-/**
- * Ajoute une entrée simple (console admin)
- */
 export function addLog(message) {
-  const entry = {
-    timestamp: new Date().toISOString(),
-    message
-  };
+  const entry = { timestamp: new Date().toISOString(), message };
   logs.unshift(entry);
-
-  // garder max 200 logs
   if (logs.length > 200) logs = logs.slice(0, 200);
 }
 
-/**
- * Récupère l’historique des logs
- */
 export function getLogs() {
   return logs;
 }
