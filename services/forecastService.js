6e55ef227b86d5b2c2c5f29d9a151f2deaa53fd2
tinsflash <skysnapia@gmail.com>
2025-10-10 14:54:55 +0200

    Update forecastService.js

--- services/forecastService.js
+++ services/forecastService.js
@@ -3,7 +3,8 @@
 // Retourne: forecast (local “now”) + localDaily[7j] + nationalDaily[7j] + alerts[]
 // Everest Protocol v3.6 — 100% réel
 
-import { SuperForecast } from "./superForecast.js";
+// services/forecastService.js
+import { runSuperForecast } from "./superForecast.js";
 import { addEngineLog, addEngineError, saveEngineState, getEngineState } from "./engineState.js";
 import Alert from "../models/Alert.js";
 
