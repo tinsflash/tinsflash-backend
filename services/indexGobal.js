1d7368bfd23bc35d55959ef3bd30daa6afa0dc1c
tinsflash <skysnapia@gmail.com>
2025-10-11 12:47:49 +0200

    Create indexGobal.js

---
+++ services/indexGobal.js
@@ -0,0 +1,18 @@
+// ==========================================================
+// 🌍 TINSFLASH – indexGlobal.js (Everest Protocol v3.14 PRO+++)
+// ==========================================================
+// Centralise l'import et l'export des runs globaux pour Render
+// ==========================================================
+
+export { runGlobalEurope } from "./runGlobalEurope.js";
+export { runGlobalUSA } from "./runGlobalUSA.js";
+export { runGlobalCanada } from "./runGlobalCanada.js";
+export { runGlobalAfricaNord } from "./runGlobalAfricaNord.js";
+export { runGlobalAfricaCentrale } from "./runGlobalAfricaCentrale.js";
+export { runGlobalAfricaOuest } from "./runGlobalAfricaOuest.js";
+export { runGlobalAfricaSud } from "./runGlobalAfricaSud.js";
+export { runGlobalAmericaSud } from "./runGlobalAmericaSud.js";
+export { runGlobalAsiaEst } from "./runGlobalAsiaEst.js";
+export { runGlobalAsiaSud } from "./runGlobalAsiaSud.js";
+export { runGlobalOceania } from "./runGlobalOceania.js";
+export { runGlobalCaribbean } from "./runGlobalCaribbean.js";
